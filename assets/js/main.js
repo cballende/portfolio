a321a550bfe419ea279c773bd7314d6005be5403
cballende <cba.allende@gmail.com>
2025-08-27 20:33:17 -0300

    sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 
 // Script to open and close sidebar
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
+function navWideOpen() {
+    document.getElementById("menuSidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
 }
  
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
+function navWideClose() {
+    document.getElementById("menuSidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
+
